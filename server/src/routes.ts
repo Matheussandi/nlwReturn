e34820d153a225a40d96a7f777e4e0a86d155462
Matheus Sandi <Matheussandi@hotmail.com>
2022-05-07 16:42:04 -0300

    SOLID application, Jest configuration and unit testing

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,34 +1,28 @@
 import express from 'express';
 import nodemailer from 'nodemailer';
+import { NodemailerMailAdapter } from './adpters/nodemailer/nodemailerMailAdapter';
 import { prisma } from './prisma';
+import { PrismaFeedbacksRepository } from './repositories/prisma/prismaFeedbacksRepository';
+import { SubmitFeedback } from './services/submitFeedback';
 
 export const routes = express.Router();
-
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: "452c7bd3ecb26b",
-      pass: "1c9d61824c0b1c"
-    }
-  });
 
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body
 
-    const feedback = 
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+    const nodemailerMailAdapter = new NodemailerMailAdapter()
 
-    await transport.sendMail({
-        from: 'Equipe Feedget <oi@feedget.com>',
-        to: 'Matheus Sandi <zTeussandi@hotmail.com>',
-        subject: 'Novo feedback',
-        html: [
-            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-            `<p>Tipo do feedback ${type}</p>`,
-            `<p>Comentário: ${comment}</p>`,
-            `</div>`
-        ].join('\n')
+    const submitFeedback = new SubmitFeedback(
+      prismaFeedbacksRepository,
+      nodemailerMailAdapter,
+    );
+
+    await submitFeedback.execute({
+      type,
+      comment,
+      screenshot,
     })
 
-    return res.status(201).json({ data: feedback });
-})
+    return res.status(201).send();
+});
